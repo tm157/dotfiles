a4cb14e6fcfe51dab6488874246ae54d6741b1fb
Tanya Marwah <tanyamarwah@Tanyas-MacBook-Air.local>
2024-03-06 12:05:19 -0500

    move to sonokai

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,35 +28,7 @@ spec "user.project"
 -- spec "user.toggleterm"
 spec "user.nvim_tmux_navigation"
 require "user.lazy"
--- vim.cmd("colorscheme sonokai")
-vim.cmd("colorscheme kanagawa-dragon")
+vim.cmd("colorscheme sonokai")
+-- vim.cmd("colorscheme kanagawa-dragon")
 -- vim.api.nvim_set_hl(0, "Normal", {bg="#1c1c1c", fg="#bcbcbc"})
-vim.api.nvim_set_hl(0, "LineNr", {bg="#1c1c1c", fg="#bcbcbc"})
--- vim.api.nvim_set_hl(0, "Normal", {bg="#1c1c1c", fg="#bcbcbc"})
-
--- -- Default options:
--- require('kanagawa').setup{
---     compile = false,             -- enable compiling the colorscheme
---     undercurl = true,            -- enable undercurls
---     commentStyle = { italic = true },
---     functionStyle = {},
---     keywordStyle = { italic = true},
---     statementStyle = { bold = true },
---     typeStyle = {},
---     transparent = false,         -- do not set background color
---     dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
---     terminalColors = true,       -- define vim.g.terminal_color_{0,17}
---     colors = {                   -- add/modify theme and palette colors
---         palette = {},
---         theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
---     },
---     overrides = function(colors) -- add/modify highlights
---         return {}
---     end,
---     theme = "wave",              -- Load "wave" theme when 'background' option is not set
---     background = {               -- map the value of 'background' option to a theme
---         dark = "wave",           -- try "dragon" !
---         light = "lotus"
---     },
--- }
---
+-- vim.api.nvim_set_hl(0, "LineNr", {bg="#1c1c1c", fg="#bcbcbc"})
